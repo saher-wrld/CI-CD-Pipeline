e9c1f48da71de3a37216f0e071ca2f06edbe7bbb
Saher Shaikh <sleepysaher@Sahers-MacBook-Air.local>
2025-05-06 14:00:14 -0400

    To to to

--- client/src/components/TestComponent.jsx
+++ client/src/components/TestComponent.jsx
@@ -4,6 +4,6 @@ import TestComponent from '../../client/src/components/TestComponent';
 describe('TestComponent', () => {
   it('renders correctly', () => {
     cy.mount(<TestComponent />);
-    cy.contains('TestComponent To test stuff');
+    cy.contains('TestComponent to test stuff');
   });
 });
