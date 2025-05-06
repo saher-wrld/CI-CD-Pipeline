924000127ef92b75db28b31acbd6ff139b9313d9
Saher Shaikh <shaikhsa@live.ca>
2025-05-06 14:28:12 -0400

    deleted test

--- cypress/component/TestComponent.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TestComponent from '../../client/src/components/TestComponent';
-
-describe('TestComponent', () => {
-  it('renders correctly', () => {
-    cy.mount(<TestComponent />);
-    cy.contains('TestComponent to test stuff');
-  });
-});
